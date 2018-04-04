e5a140df9b7254b8b787083df859ebb7a4c501e7
cryosx <cyruswu.email@gmail.com>
2018-04-04 11:12:57 -1000

    Fill Finished

--- js/PixelPainter.js
+++ js/PixelPainter.js
@@ -1,45 +1,57 @@
 function PixelPainter(width, height) {
   const _width = width;
   const _height = height;
-  let brush = null;
+  const defaultColor = 'white';
+  let brush = defaultColor;
+  let fillState = false;
 
   return {
-    aseemble: assemble,
+    build: build,
     getRandomHexColor: getRandomHexColor
   };
 
-  function assemble() {
+  function build() {
     let canvas = document.getElementById('pixelPainter');
-
-    let swatch = buildGrid('swatch', 'swatchCell', 12, 8);
-    setupSwatch(swatch);
-    canvas.appendChild(swatch);
 
     let selectColor = document.createElement('div');
     selectColor.id = 'selectColor';
     canvas.appendChild(selectColor);
 
-    let clearBtn = document.createElement('button');
-    clearBtn.id = 'clearButton';
-    clearBtn.innerText = 'Clear';
-    clearBtn.addEventListener('click', clearGrid);
-    canvas.appendChild(clearBtn);
+    let swatch = buildGrid('swatch', 'swatchCell', 8, 12);
+    setupSwatchCells(swatch);
+    canvas.appendChild(swatch);
 
-    let eraseBtn = document.createElement('button');
-    eraseBtn.id = 'eraseButton';
-    eraseBtn.innerText = 'Erase';
-    eraseBtn.addEventListener('click', function() {
-      brush = 'white';
-      document.getElementById('selectColor').style.background = brush;
-    });
-    canvas.appendChild(eraseBtn);
+    canvas.appendChild(createButton(clearGrid, 'clearButton', 'Clear'));
+    canvas.appendChild(
+      createButton(
+        function() {
+          brush = defaultColor;
+          document.getElementById('selectColor').style.background = brush;
+        },
+        'eraseButton',
+        'Erase'
+      )
+    );
+    canvas.appendChild(createButton(toggleFill, 'fillButton', 'Fill'));
 
     let grid = buildGrid('grid', 'gridCell', _width, _height);
-    setupGrid(grid);
+    setupGridCells(grid);
     canvas.appendChild(grid);
   }
 
-  function setupSwatch(swatch) {
+  function createButton(func, id, text) {
+    let btn = document.createElement('button');
+    btn.id = id;
+    btn.innerText = text;
+    btn.addEventListener('click', func);
+    return btn;
+  }
+
+  function toggleFill() {
+    fillState = !fillState;
+  }
+
+  function setupSwatchCells(swatch) {
     let cells = swatch.querySelectorAll('div.swatchCell');
     for (let i = 0; i < cells.length; i++) {
       cells[i].style = 'background: #' + getRandomHexColor();
@@ -52,20 +64,70 @@ function PixelPainter(width, height) {
     }
   }
 
-  function setupGrid(grid) {
+  function setupGridCells(grid) {
     let cells = grid.querySelectorAll('div.gridCell');
     // console.log(cells);
     for (let i = 0; i < cells.length; i++) {
       cells[i].addEventListener('click', function() {
         // console.log(this.style.background);
-        cells[i].style = 'background: ' + brush;
+        // console.log(arguments[0]);
+        // console.log(arguments[0].path[0].classList[2]);
+        // console.log(arguments[0].path[1].classList[1]);
+
+        if (fillState) {
+          console.log(arguments[0].path);
+          fill(
+            arguments[0].path[0].classList[2],
+            arguments[0].path[1].classList[1]
+          );
+        }
+        cells[i].style.background = brush;
       });
       cells[i].addEventListener('mousemove', function() {
-        console.log(arguments[0]);
+        // console.log(arguments[0]);
         if (arguments[0].buttons === 1) {
-          cells[i].style = 'background: ' + brush;
+          cells[i].style.background = brush;
         }
       });
+    }
+  }
+
+  function fill(col, row) {
+    let rowNum = Number.parseInt(row);
+    let colNum = Number.parseInt(col);
+    let cells = document.querySelectorAll('div.gridCell');
+    let targetColor = cells[getCellPosition(rowNum, colNum)].style.background;
+    console.log(targetColor);
+
+    // console.log(cells[1]);
+    // console.log(rowNum * _width + colNum);
+    console.log(cells[rowNum * _width + colNum]);
+    // console.log(cell + ' : ' + row);
+    fillHelper(cells, rowNum, colNum, targetColor);
+  }
+
+  function fillHelper(cells, row, col, targetColor) {
+    // debugger;
+    console.log(targetColor);
+    console.log(cells[getCellPosition(row, col)]);
+    if (
+      getCellPosition(row, col) >= 0 &&
+      getCellPosition(row, col) <= _height * _width - 1
+    ) {
+      let cell = cells[getCellPosition(row, col)];
+      if (cell.style.background === targetColor) {
+        cell.style.background = brush;
+        fillHelper(cells, row - 1, col, targetColor);
+        fillHelper(cells, row, col + 1, targetColor);
+        fillHelper(cells, row + 1, col, targetColor);
+        fillHelper(cells, row, col - 1, targetColor);
+      }
+    }
+  }
+
+  function getCellPosition(row, col) {
+    if (typeof col === 'number' && typeof row === 'number') {
+      return row * _width + col;
     }
   }
 
@@ -73,12 +135,13 @@ function PixelPainter(width, height) {
     let container = document.createElement('div');
     container.id = containerName;
 
-    for (let i = 0; i < gridWidth; i++) {
+    for (let i = 0; i < gridHeight; i++) {
       let row = document.createElement('div');
       row.className = 'row ' + i;
-      for (let j = 0; j < gridHeight; j++) {
+      for (let j = 0; j < gridWidth; j++) {
         let cell = document.createElement('div');
         cell.className = cellName + ' col ' + j;
+        cell.style.background = defaultColor;
         row.appendChild(cell);
       }
       container.appendChild(row);
@@ -91,7 +154,7 @@ function PixelPainter(width, height) {
     let cells = grid.querySelectorAll('div.gridCell');
     // console.log(cells);
     for (let i = 0; i < cells.length; i++) {
-      cells[i].style.background = 'white';
+      cells[i].style.background = defaultColor;
     }
   }
 
@@ -104,8 +167,11 @@ function PixelPainter(width, height) {
     }
     return color;
   }
+  function getHSLGraident() {
+    let hslColors = [];
+  }
 }
 
 let pp = PixelPainter(10, 10);
-pp.aseemble();
+pp.build();
 // document.body.style = 'background: #' + pp.getRandomHexColor();
